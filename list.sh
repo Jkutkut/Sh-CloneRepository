212ff7ba13fe25bbc4077d500d46fbe85ebe5a60
jkutkut <jorgeregonzalez@gmail.com>
2020-10-15 16:19:30 +0200

    basic display made. Fill it with info missing

--- list.sh
+++ list.sh
@@ -1,31 +1,65 @@
 #!/bin/sh
 
+# colors
+NC='\033[0m' # No Color
+sBG='\e[1;7m';
+rBG='\e[1;41m';
+bBG='\e[1;44m';
+gBG='\e[1;42m';
+
+TITLE='\033[38;5;33m';
+
 # functions
-print_option()     { printf "   $1 "; }
-print_selected()   { printf "  \e[1;7m$1\e[0m"; }
+print_option()     { printf "   $1                       "; }
+print_selected()   { printf "   ${sBG}$1${NC}                       ";}
 
 # variables
-height=$(($(tput lines)-1));
+titleH=4;
+titleSpace=2;
+height=$(($(tput lines)-1-$titleH-$titleSpace-1));
 
 
 # code
-for i in `seq 0 $height`; do printf "\n"; done
+for i in `seq 0 $(tput lines)`; do printf "\n"; done
+tput cup 0;
+printf "${TITLE} ____  ____  ____   __        __    __  ____  ____ 
+(  _ \(  __)(  _ \ /  \  ___ (  )  (  )/ ___)(_  _)
+ )   / ) _)  ) __/( () )(___)/ (_/\ )( \___ \  )(  
+(__\_)(____)(__)   \__/      \____/(__)(____/ (__)${NC}"
 
 trap "cursor_blink_on; stty echo; printf '\n'; exit" 2
-# setterm -cursor off; # cursor_blink_off
+setterm -cursor off; # cursor_blink_off
+
+
+
+# OP=(hola que tal);
 
 selected=2;
 while true; do
     idx=0;
     for i in `seq 0 $height`; do
-        tput cup $idx;
+        tput cup $(($titleH+$idx+$titleSpace));
         if [ $i -eq $selected ]; then
-            # print_selected "here";
-            print_selected "hola"
+            print_selected "here"
         else
             print_option "$i";
         fi
         idx=$(($idx+1));
     done
+
+    # user key control
+    k=$(./keyInput.sh);
+    # echo "k = $k"
+    case $k in
+        EN) break;;
+        UP) 
+            selected=$(($selected-1));
+            if [ $selected -lt 0 ]; then selected=$(($height)); fi;;
+        DN)
+            selected=$(($selected+1));
+            if [ $selected -ge $(($height+1)) ]; then selected=0; fi;;
+    esac
+    # break;
 done
-# setterm -cursor on; # cursor_blink_on
+echo "";
+setterm -cursor on; # cursor_blink_on
