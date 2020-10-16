81a2cdce1e0f65897f51bd3eb67d2aa668f6f9b7
jkutkut <jorgeregonzalez@gmail.com>
2020-10-17 00:56:38 +0200

    getline function added on this file

--- list.sh
+++ list.sh
@@ -20,8 +20,13 @@ setMessage(){
     gap=$(($l-${#1}-$offs)); # characters I can fit between the end of the message and the end of the terminal
     
     printf %${offs}s; # Print offset
-    printf $4$1${NC}; # print message with the color given
+    printf $3$1${NC}; # print message with the color given
     printf %${gap}s; # print the end of the line with blank characters
+}
+getLine(){
+    start=$2;
+    get=$3;
+    echo $(tail -n +$start $1 | head -n $get);
 }
 
 # variables
@@ -32,7 +37,7 @@ height=$(($(tput lines)-1-$titleH-$titleSpace-1));
 
 # code
 for i in `seq 0 $(tput lines)`; do printf "\n"; done # Clear the terminal
-tput cup 0;
+tput cup 0; # Set cursor on the top of the screen
 printf "${TITLE} ____  ____  ____   __        __    __  ____  ____ 
 (  _ \(  __)(  _ \ /  \  ___ (  )  (  )/ ___)(_  _)
  )   / ) _)  ) __/( () )(___)/ (_/\ )( \___ \  )(  
@@ -40,27 +45,34 @@ printf "${TITLE} ____  ____  ____   __        __    __  ____  ____
 
 trap "cursor_blink_on; stty echo; printf '\n'; exit" 2
 setterm -cursor off; # cursor_blink_off
+stty -echo;
 
 # Get repo names, store them on a temporal file
-curl -u jkutkut:9f3097b9f4e189766e98c92775743249aa6d565f -s "https://api.github.com/users/jkutkut/repos?type=all&per_page=100" |
-jq '.[]|.full_name' | cut -d'/' -f 2 | sed 's/.$//' >> temp.txt;
-REPOS=$(tail -n +$start temp.txt | head -n $get);
+/usr/bin/printf "\n\nGetting al the repos:";
+# curl -u  -s "https://api.github.com/users/jkutkut/repos?type=all&per_page=100" |
+# jq '.[]|.full_name' | cut -d'/' -f 2 | sed 's/.$//' >> temp.txt;
+/usr/bin/printf '\r\033[0;32m\xE2\x9C\x94\033[0m All repositories obtained:\n';
 
 
 
 # OP=(hola que tal);
+
 
 selected=2;
 while true; do
     idx=0;
     for i in `seq 0 $height`; do
         tput cup $(($titleH+$idx+$titleSpace));
+        index=$(($i+1));
+        text=$(getLine temp.txt $index 1);
+
+
         if [ $i -eq $selected ]; then
-            printf "${sBG}";
-            setMessage "here" 3 $sBG;
+            # printf "${sBG}";
+            setMessage $text 3 $sBG;
             printf "${NC}";
         else
-            setMessage $i 3;
+            setMessage $text 3;
         fi
         idx=$(($idx+1));
     done
@@ -79,4 +91,5 @@ while true; do
 done
 echo "";
 setterm -cursor on; # cursor_blink_on
-rm temp.txt; # Remove temporal file
+stty echo;
+# rm temp.txt; # Remove temporal file
